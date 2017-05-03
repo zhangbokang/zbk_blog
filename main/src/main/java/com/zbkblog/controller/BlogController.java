73ed49cd051da54aac7d19d4bf43878e8b106dd5
zhangbokang <540064991@qq.com>
2017-05-03 23:19:53 +0800

    添加中文乱码处理过滤器。

--- main/src/main/java/com/zbkblog/controller/BlogController.java
+++ main/src/main/java/com/zbkblog/controller/BlogController.java
@@ -2,14 +2,21 @@ package com.zbkblog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by zhangbokang on 2017/5/3.
  */
 @Controller
 public class BlogController {
-    @RequestMapping("index")
+    @RequestMapping
     public String index(){
         return "index";
+    }
+
+    @RequestMapping
+    @ResponseBody
+    public String abc(){
+        return "哈哈";
     }
 }

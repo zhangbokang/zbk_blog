caf6eb3395e90e21c1add9b35dc53e6b90c5c722
zhangbokang <zhangbokang@himto.cn>
2017-05-15 19:06:54 +0800

    测试

---
+++ database/create_db.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE `zbk_blog` /*!40100 COLLATE 'utf8_general_ci' */
--- main/src/main/java/com/zbkblog/test/MyTest.java
+++ main/src/main/java/com/zbkblog/test/MyTest.java
@@ -4,10 +4,14 @@ import com.zbkblog.entity.Doc;
 import com.zbkblog.utils.HibernateSessionUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.hibernate.type.LongType;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 /**
  * Created by zhangbokang on 2017/5/13.
@@ -30,12 +34,30 @@ public class MyTest {
         //System.out.println(System.currentTimeMillis()/1000);
         //System.out.println(HibernateSessionUtil.getSession());
         Session session = HibernateSessionUtil.getSession();
-        Doc doc = session.get(Doc.class,Long.valueOf(1494777977));
-        System.out.println("Title:"+doc.getTitle());
-        System.out.println("DocMd:"+doc.getDocMd());
-        System.out.println("Classify:"+doc.getClassify().getName());
-        System.out.println("Tag:"+doc.getTag().getName());
+        //Doc doc1 = session.load(Doc.class,Long.valueOf(1494777977));
+        String hql = "from Doc where docId=?";
+        Query query = session.createQuery(hql);
+        query.setParameter(0,Long.valueOf(1494777977), LongType.INSTANCE);
+        List<Doc> list = query.list();
+        for (Doc doc:list) {
+            System.out.println("Title:"+doc.getTitle());
+            System.out.println("DocMd:"+doc.getDocMd());
+            System.out.println("ClassifyId:"+doc.getClassify().getClassifyId());
+            System.out.println("TagId:"+doc.getTag().getTagId());
+        }
+
         session.close();
+    }
+
+    @Test
+    public void testInster(){
+        Session session = HibernateSessionUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        Doc doc = new Doc();
+        doc.setTitle("jfkdalfjdsf");
+        doc.setDocMd("#这里是md");
+        doc.
+
     }
 
 }

9ba3e2cbf9e8eb564eff81658b08c31682faff7c
zhangbokang <zhangbokang@himto.cn>
2017-06-03 20:20:22 +0800

    新增用户表的数据库信息和sql脚本。

---
+++ database/zbk_blog_db_user.sql
@@ -0,0 +1,22 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     2017/6/3 20:18:22                            */
+/*==============================================================*/
+
+
+drop table if exists blog_user;
+
+/*==============================================================*/
+/* Table: blog_user                                             */
+/*==============================================================*/
+create table blog_user
+(
+   user_id              bigint(13) not null comment '用户ID',
+   user_name            varchar(20) binary not null comment '用户名',
+   password             varchar(20) binary comment '用户密码',
+   status               varchar(1) not null comment '用户状态：0 禁用，1 启用',
+   primary key (user_id)
+);
+
+alter table blog_user comment '可以编辑系统的表';
+

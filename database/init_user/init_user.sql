f843504cf29cc75e834a33156207e61e342cc438
zhangbokang <540064991@qq.com>
2018-07-11 21:54:50 +0800

    微调项目，独立出用户初始化sql语句

---
+++ database/init_user/init_user.sql
@@ -0,0 +1,5 @@
+/*初始用户
+    用户zbk
+    密码Zhang123
+*/
+INSERT INTO `zbk_blog`.`blog_user` (`user_id`, `user_name`, `password`, `status`) VALUES ('1000000000001', 'zbk', '648727186f3dc12ac987605722e47d09', '1');

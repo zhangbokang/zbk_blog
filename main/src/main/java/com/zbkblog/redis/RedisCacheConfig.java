1c9073cd16fb4a69d91a054d2a04b1c7413bbc7f
zhangbokang <540064991@qq.com>
2018-08-31 16:54:07 +0800

    增加redis缓存服务器出现问题时仍然可以提供服务。

--- main/src/main/java/com/zbkblog/redis/RedisCacheConfig.java
+++ main/src/main/java/com/zbkblog/redis/RedisCacheConfig.java
@@ -1,14 +1,18 @@
 package com.zbkblog.redis;
 
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.logging.Logger;
 
 
 /**
@@ -18,6 +22,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 @EnableCaching
 public class RedisCacheConfig extends CachingConfigurerSupport {
+
+    private Logger logger = Logger.getLogger(RedisCacheConfig.class.getName());
 
     private volatile JedisConnectionFactory mJedisConnectionFactory;
     private volatile RedisTemplate<String, String> mRedisTemplate;
@@ -45,6 +51,41 @@ public class RedisCacheConfig extends CachingConfigurerSupport {
 
     public CacheManager cacheManager(RedisTemplate<?, ?> redisTemplate) {
         return mRedisCacheManager;
+    }
+
+    @Override
+    public CacheErrorHandler errorHandler() {
+        CacheErrorHandler cacheErrorHandler = new CacheErrorHandler() {
+
+            @Override
+            public void handleCachePutError(RuntimeException exception, Cache cache,
+                                            Object key, Object value) {
+                RedisErrorException(exception, key);
+            }
+
+            @Override
+            public void handleCacheGetError(RuntimeException exception, Cache cache,
+                                            Object key) {
+                RedisErrorException(exception, key);
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException exception, Cache cache,
+                                              Object key) {
+                RedisErrorException(exception, key);
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException exception, Cache cache) {
+                RedisErrorException(exception, null);
+            }
+        };
+        return cacheErrorHandler;
+    }
+
+    protected void RedisErrorException(Exception exception,Object key){
+        logger.info("redis异常：key=["+key+"]");
+        exception.printStackTrace();
     }
 
     //将key生成策略换成自定义的
